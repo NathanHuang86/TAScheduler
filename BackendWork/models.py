750f2ca677c2aa29b862922321809d7a2d4dd388
NathanHuang86 <78175507+NathanHuang86@users.noreply.github.com>
2022-04-22 15:36:01 -0500

    Fixes for Files

--- BackendWork/models.py
+++ BackendWork/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
@@ -8,21 +9,21 @@ class User(models.Model):
     address = models.CharField(max_length=72)
     phone = models.CharField(max_length=10)
 
+
 class Class(models.Model):
     name = models.CharField(max_length=20)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
-class Role(models.Model):
-    API_Name = models.CharField
-    label = models.CharField
 
 class Permission(models.Model):
     API_Name = models.CharField
     label = models.CharField
 
+
 class PermissionAssignment(models.Model):
     userID = models.ForeignKey(User)
     permissionAPIName = models.ForeignKey(Permission)
+
 
 class Schedule(models.Model):
     sunday = models.BooleanField
@@ -35,13 +36,10 @@ class Schedule(models.Model):
     startTime = models.TimeField
     endTime = models.TimeField
 
+
 class Section(models.Model):
     Class = models.ForeignKey(Class)
     TA = models.ForeignKey(User)
     schedule = models.ForeignKey(Schedule)
     time = models.TimeField
     sectionNumber = models.IntegerField
-
-class SectionType(models.Model):
-    API_Name = models.CharField
-    label = models.CharField
