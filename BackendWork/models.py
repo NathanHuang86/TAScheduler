fdbe897f64ba2b9510857ce8dee472fd236f21be
ErikWenslaff <90141934+ErikWenslaff@users.noreply.github.com>
2022-04-23 23:02:12 -0500

    updated models.py

--- BackendWork/models.py
+++ BackendWork/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class User(models.Model):
+    ROLE_CHOICES = (('Teaching Assistant', 'TEACHING_ASSISTANT'), ('Instructor', 'INSTRUCTOR'), ('Admin', 'ADMIN'))
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
     address = models.CharField(max_length=72)
     phone = models.CharField(max_length=10)
+    role = models.Choices(max_length=20, choices=ROLE_CHOICES)
 
 
 class Class(models.Model):
@@ -22,7 +24,7 @@ class Permission(models.Model):
 
 class PermissionAssignment(models.Model):
     userID = models.ForeignKey(User)
-    permissionAPIName = models.ForeignKey(Permission)
+    permission = models.ForeignKey(Permission)
 
 
 class Schedule(models.Model):
@@ -38,8 +40,10 @@ class Schedule(models.Model):
 
 
 class Section(models.Model):
+    SECTION_TYPE_CHOICES = (('Discussion', 'DISCUSSION'), ('Lab', 'LAB'))
     Class = models.ForeignKey(Class)
     TA = models.ForeignKey(User)
     schedule = models.ForeignKey(Schedule)
     time = models.TimeField
     sectionNumber = models.IntegerField
+    sectionType = models.CharField(max_length=20, choices=SECTION_TYPE_CHOICES)
