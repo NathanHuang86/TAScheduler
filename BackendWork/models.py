cbb192b51d8a151fee275cfcb1ab749a9138d61d
ErikWenslaff <90141934+ErikWenslaff@users.noreply.github.com>
2022-04-25 08:56:41 -0500

    fixed models.py, got the unit tests running

--- BackendWork/models.py
+++ BackendWork/models.py
@@ -1,63 +1,31 @@
 from django.db import models
+from multiselectfield import MultiSelectField
+from _datetime import time
 
 
 class MyUser(models.Model):
+    ROLE_CHOICES = (('Teaching Assistant', 'TEACHING_ASSISTANT'), ('Instructor', 'INSTRUCTOR'), ('Admin', 'ADMIN'))
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
     address = models.CharField(max_length=72)
     phone = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.name
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, default='Teaching Assistant')
 
 
 class ClassList(models.Model):
     name = models.CharField(max_length=20)
-    owner = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Permission(models.Model):
-    API_Name = models.CharField
-    label = models.CharField
-
-    def __str__(self):
-        return self.name
-
-
-class PermissionAssignment(models.Model):
-    userID = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    permissionAPIName = models.ForeignKey(Permission, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-class Schedule(models.Model):
-    sunday = models.BooleanField
-    monday = models.BooleanField
-    tuesday = models.BooleanField
-    wednesday = models.BooleanField
-    thursday = models.BooleanField
-    friday = models.BooleanField
-    saturday = models.BooleanField
-    startTime = models.TimeField
-    endTime = models.TimeField
-
-    def __str__(self):
-        return self.name
+    owner = models.ForeignKey(MyUser, on_delete=models.CASCADE, blank=True, null=True)
 
 
 class Section(models.Model):
-    Class = models.ForeignKey(ClassList, on_delete=models.CASCADE)
-    TA = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
-    time = models.TimeField
-    sectionNumber = models.IntegerField
-
-    def __str__(self):
-        return self.name
+    SCHEDULE_CHOICES = (('Sunday', 'SUNDAY'), ('Monday', 'MONDAY'), ('Tuesday', 'TUESDAY'), ('Wednesday', 'WEDNESDAY'), ('Thursday', 'THURSDAY'), ('Friday', 'FRIDAY'), ('Saturday', 'SATURDAY'))
+    SECTION_TYPE_CHOICES = (('Discussion', 'DISCUSSION'), ('Lab', 'LAB'))
+    Class = models.ForeignKey(ClassList, on_delete=models.SET_NULL, null=True)
+    TA = models.ForeignKey(MyUser, on_delete=models.SET_NULL, null=True)
+    sectionNumber = models.IntegerField(default=0)
+    sectionType = models.CharField(max_length=30, choices=SECTION_TYPE_CHOICES, default='Discussion')
+    schedule = MultiSelectField(max_length=30, choices=SCHEDULE_CHOICES)
+    startTime = models.TimeField(default=time(0, 0, 0))
+    endTime = models.TimeField(default=time(0, 0, 0))
