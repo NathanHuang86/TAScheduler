26b2ed4e73f1b75864b009608a67c7ab4811d544
ErikWenslaff <90141934+ErikWenslaff@users.noreply.github.com>
2022-04-25 22:56:49 -0500

    fixed unit tests

--- BackendWork/unit_tests/test_myuser.py
+++ BackendWork/unit_tests/test_myuser.py
@@ -14,42 +14,48 @@ class MyUserUnitTestSuite(TestCase):
         adminList = MyUser.objects.filter(role='Admin')
         instructorList = MyUser.objects.filter(role='Instructor')
         teachingAssistantList = MyUser.objects.filter(role='Teaching Assistant')
-        self.assertEqual(len(adminList), 2)
+        self.assertEqual(len(adminList), 2, msg='MyUser failed to filter by role')
+        self.assertEqual(len(instructorList), 2, msg='MyUser failed to filter by role')
+        self.assertEqual(len(teachingAssistantList), 2, msg='MyUser failed to filter by role')
 
     def test_list_myuser_names(self):
-        charlieClassList = ClassList.objects.filter(owner=self.charlie)
-        self.assertEqual(charlieClassList, [self.science, self.english])
+        adminList = list(MyUser.objects.filter(role='Admin'))
+        instructorList = list(MyUser.objects.filter(role='Instructor'))
+        teachingAssistantList = list(MyUser.objects.filter(role='Teaching Assistant'))
+        self.assertEqual(adminList, [self.charlie, self.dennis], msg='MyUser failed to filter by role')
+        self.assertEqual(instructorList, [self.bob, self.ellis], msg='MyUser failed to filter by role')
+        self.assertEqual(teachingAssistantList, [self.alice, self.felix], msg='MyUser failed to filter by role')
 
     def test_edit_username(self):
-        self.history.owner = self.charlie
-        self.assertEqual(self.charlie, self.history.owner)
+        self.alice.username = 'arial'
+        self.assertEqual('arial', self.alice.username, msg='MyUser failed to edit username')
 
     def test_edit_password(self):
-        self.history.name = 'US history'
-        self.assertEqual('US history', self.history.name)
+        self.alice.password = 'password@'
+        self.assertEqual('password@', self.alice.password, msg='MyUser failed to edit password')
 
     def test_edit_name(self):
-        self.history.name = 'US history'
-        self.assertEqual('US history', self.history.name)
+        self.alice.name = 'arial'
+        self.assertEqual('arial', self.alice.name, msg='MyUser failed to edit name')
 
     def test_edit_email(self):
-        self.history.name = 'US history'
-        self.assertEqual('US history', self.history.name)
+        self.alice.name = 'alice@gmail.com'
+        self.assertEqual('alice@gmail.com', self.alice.name, msg='MyUser failed to edit email')
 
     def test_edit_address(self):
-        self.history.name = 'US history'
-        self.assertEqual('US history', self.history.name)
+        self.alice.address = 'nowhere'
+        self.assertEqual('nowhere', self.alice.address, msg='MyUser failed to edit address')
 
     def test_edit_phone(self):
-        self.history.name = 'US history'
-        self.assertEqual('US history', self.history.name)
+        self.alice.phone = '0987654321'
+        self.assertEqual('0987654321', self.alice.phone, msg='MyUser failed to edit phone')
 
     def test_delete_myuser(self):
-        self.science.delete()
-        charlieClassList = ClassList.objects.filter(owner=self.charlie)
-        self.assertEqual(1, len(charlieClassList))
+        self.alice.delete()
+        teachingAssistantList = MyUser.objects.filter(role='Teaching Assistant')
+        self.assertEqual(1, len(teachingAssistantList), msg='MyUser failed to delete a record')
 
     def test_create_myuser(self):
-        self.english2 = ClassList.objects.create(name='english2', owner=self.charlie)
-        charlieClassList = ClassList.objects.filter(owner=self.charlie)
-        self.assertEqual(3, len(charlieClassList))
+        self.garry = MyUser.objects.create(username='garry', password='password!#', name='garry', email='garry@yahoo.com', address='middleonowhere', phone='123-456-7896', role='Admin')
+        adminList = MyUser.objects.filter(role='Admin')
+        self.assertEqual(3, len(adminList), msg='MyUser failed to create a record')
